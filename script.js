1ac52a7ca0a71ece7cff6a91b8ea3605994f7e84
chandu399341 <guruchandra034@gmail.com>
2026-02-26 14:38:28 +0530

    added

---
+++ script.js
@@ -0,0 +1,18 @@
+let obj1={
+    name:'abc',
+    age:22
+}
+let obj2=obj1
+obj2.name='xyz';
+console.log(obj1.name,obj1.age)
+
+
+console.log(5+'5');
+console.log(5-'5');
+console.log(5=='5');
+console.log(5==='5');
+
+
+let arr=[1,'new',null]
+console.log(arr);
+
