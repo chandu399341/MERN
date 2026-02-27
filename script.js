62286fce42bf1003559db29f659fe7b662cccc16
chandu399341 <guruchandra034@gmail.com>
2026-02-27 10:05:55 +0530

    hlo

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ console.log(5!='5')
 let arr=[1,'new',null]
 console.log(arr);
 
+
+
+
+let mark =30
+if(mark<=40){
+    console.log('pass')
+}else{
+    console.log('fail')
+}
+
+
+let res = mark>=40?'fail':'pass'
+console.log(res);
+
+// Even or Odd
+let n=5
+if(n%2==0){
+    console.log('even');
+}else{
+    console.log('odd');
+    
+}
+
+//Using Ternary Operator
+let r=n%2==0?'even':'odd'
+console.log(r);
+
+//Bitwise Operators
+console.log(10&6); //And
+console.log(10|6); //or
+console.log(10^6); //Xor
+console.log(~6); //Not
+console.log(6>>1);
+console.log(6<<1);
+
+
+
+
+
+
+
+
+
